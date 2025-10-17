13a520fc1104cd969247fb0f0f1bfc96bd151b92
Son Le Thanh <sonlt@kth.se>
2025-10-17 18:10:52 +0200

    Some updates

---
+++ graphFromIvw.py
@@ -0,0 +1,114 @@
+import numpy as np 
+
+class IvwCriticalPoints:
+    def __init__(self, idx_wpc, val, node_type):
+        self.idx_wpc = idx_wpc
+        self.val = val
+        self.node_type = node_type
+        # Maybe save the graph idx?
+        self.idx_graph = -1
+        self.pos = np.zeros(3)
+
+    def __str__(self):
+        return f"Node {self.idx_wpc} of type {self.node_type}, val {self.val}, idx in graph {self.idx_graph}, pos ({self.pos[0]}, {self.pos[1]}, {self.pos[2]})"
+
+# Read the data from the file
+def read_graph_info(in_file):
+    results = []
+
+    all_idx_wpc = {}
+    minima_idx = []
+    saddle_idx = []
+    maxima_idx = []
+
+    all_nodes = []
+
+    idx_graph_counter = 0
+    with open(in_file, "r") as file:
+        for line in file:
+            parts = line.split(",")
+            # The first one is the lenght of the separatrices
+            sepa_length = int(parts[0])
+            
+            # After that we have the node info
+            first_node_info = parts[1].split(" ")
+            first_node = IvwCriticalPoints(int(first_node_info[0]), float(first_node_info[1]), int(first_node_info[2]))
+            # Set the correct graph index
+            if first_node.idx_wpc not in all_idx_wpc:
+                # This node is new
+                all_idx_wpc[first_node.idx_wpc] = idx_graph_counter
+                match first_node.node_type:
+                    case 0:
+                        minima_idx.append(idx_graph_counter)
+                    case 1:
+                        saddle_idx.append(idx_graph_counter)
+                    case 2:
+                        maxima_idx.append(idx_graph_counter)
+                    case _:
+                        # Do nothing
+                        print("Not supporting atm")
+
+                idx_graph_counter += 1
+                all_nodes.append(first_node)
+            # Update the idx graph
+            first_node.idx_graph = all_idx_wpc[first_node.idx_wpc]
+
+            # Update the positions
+            first_node_pos = parts[3].split(" ")
+            first_node.pos = np.array([float(first_node_pos[0]), float(first_node_pos[1]), float(first_node_pos[2])])
+
+            second_node_info = parts[2].split(" ")
+            second_node = IvwCriticalPoints(int(second_node_info[0]), float(second_node_info[1]), int(second_node_info[2]))
+            # Set the correct graph index
+            if second_node.idx_wpc not in all_idx_wpc:
+                # This node is new
+                all_idx_wpc[second_node.idx_wpc] = idx_graph_counter
+                match second_node.node_type:
+                    case 0:
+                        minima_idx.append(idx_graph_counter)
+                    case 1:
+                        saddle_idx.append(idx_graph_counter)
+                    case 2:
+                        maxima_idx.append(idx_graph_counter)
+                    case _:
+                        # Do nothing
+                        print("Not supporting atm")
+                idx_graph_counter += 1
+                all_nodes.append(second_node)
+            # Update the idx graph
+            second_node.idx_graph = all_idx_wpc[second_node.idx_wpc]
+
+            # Update the positions
+            second_node_pos = parts[4].split(" ")
+            second_node.pos = np.array([float(second_node_pos[0]), float(second_node_pos[1]), float(second_node_pos[2])])
+
+            # Add to the result
+            results.append([sepa_length, first_node, second_node])
+
+    for node in all_nodes:
+        print(node)
+
+    # Also keep track of the number of nodes in the graph
+    return results, idx_graph_counter, minima_idx, saddle_idx, maxima_idx
+
+# Create an adjacency matrix from the list
+def create_adj_matrix_from_ivw(in_file, use_sepa_length=False):
+    edges, num_nodes, minima_idx, saddle_idx, maxima_idx = read_graph_info(in_file)
+
+    # Create a zero matrix
+    adj_matrix = np.zeros((num_nodes, num_nodes))
+
+    # Loop for every read separatrices
+    for sepa in edges:
+        # Edge length
+        sepa_length = sepa[0]
+        # Node info
+        first_node_info = sepa[1]
+        second_node_info = sepa[2]
+        # Node weight
+        sepa_weight = abs(first_node_info.val - second_node_info.val)
+
+        # Mark the adj_matrix
+        adj_matrix[first_node_info.idx_graph, second_node_info.idx_graph] = adj_matrix[second_node_info.idx_graph, first_node_info.idx_graph] = sepa_weight * sepa_length if use_sepa_length else sepa_weight
+
+    return adj_matrix, minima_idx, saddle_idx, maxima_idx
---
+++ runIvw1.py
@@ -0,0 +1,133 @@
+from graphHKS import *
+import numpy as np
+from scipy.sparse.linalg import eigsh
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import cdist, cosine
+
+from utils import symmetrize_matrix, popoluate_adjacency_matrix
+from mpl_toolkits.mplot3d import Axes3D  # needed for older Matplotlib vers
+from graphFromIvw import create_adj_matrix_from_ivw
+
+
+file1 = "IvwData/Case1/Timestep1.txt"
+file2 = "IvwData/Case1/Timestep2.txt"
+
+# t_hks = np.linspace(0.01, 80, 1000)
+t_hks = np.logspace(1, 10, 100)
+
+is_normalized_Laplacian = True
+is_normalize_hks = True
+
+use_sepa_length = False
+
+
+# Get the adjacency matrix
+adj_mat_t1, minima_idx1, saddle_idx1, maxima_idx1 = create_adj_matrix_from_ivw(file1, use_sepa_length)
+adj_mat_t2, minima_idx2, saddle_idx2, maxima_idx2 = create_adj_matrix_from_ivw(file2, use_sepa_length)
+
+num_eigen = adj_mat_t1.shape[0] + 1
+# Compute the eigen values
+eigen_val1, eigen_vec1, laplacian1 = compute_eigen_laplacian(adj_mat_t1, num_eigen, is_normalized_Laplacian)
+eigen_val2, eigen_vec2, laplacian2 = compute_eigen_laplacian(adj_mat_t2, num_eigen, is_normalized_Laplacian)
+
+# Compute the hks
+hks1 = compute_graph_hks(eigen_val1, eigen_vec1, t_hks, is_normalize_hks)
+hks2 = compute_graph_hks(eigen_val2, eigen_vec2, t_hks, is_normalize_hks)
+
+# minima_idx1.pop()
+# maxima_idx2.pop(1)
+
+
+# We want to only compare the critical points with the same type
+hks1_min    = hks1[minima_idx1]
+hks1_saddle = hks1[saddle_idx1]
+hks1_max    = hks1[maxima_idx1]
+
+
+hks2_min    = hks2[minima_idx2]
+hks2_saddle = hks2[saddle_idx2]
+hks2_max    = hks2[maxima_idx2]
+
+# print(hks1_min.shape)
+
+# Compute the differences between the points
+dist_min_e    = cdist(hks1_min,       hks2_min,       metric="cityblock")
+dist_max_e    = cdist(hks1_max,       hks2_max,       metric="cityblock")
+dist_saddle_e = cdist(hks1_saddle,    hks2_saddle,    metric="cityblock")
+
+
+dist_min_cosine    = cdist(hks1_min,       hks2_min,       metric="cosine")
+dist_max_cosine    = cdist(hks1_max,       hks2_max,       metric="cosine")
+dist_saddle_cosine = cdist(hks1_saddle,    hks2_saddle,    metric="cosine")
+
+# Plot heatmap
+val_min = min(hks1.min(), hks2.min())
+val_max = max(hks1.max(), hks2.max())
+
+
+plt.figure(figsize=(8, 6))
+plt.imshow(dist_min_e, cmap='viridis', interpolation='nearest')
+plt.colorbar(label='Euclidean distance of HKS - Min')
+plt.xlabel('Graph 2')
+plt.ylabel('Graph 1')
+plt.title('Pairwise Distances Heatmap')
+# plt.xticks(range(11))
+# plt.yticks(range(11))
+plt.show()
+
+
+plt.figure(figsize=(8, 6))
+plt.imshow(dist_saddle_e, cmap='viridis', interpolation='nearest')
+plt.colorbar(label='Euclidean distance of HKS - Saddles')
+plt.xlabel('Graph 2')
+plt.ylabel('Graph 1')
+plt.title('Pairwise Distances Heatmap')
+# plt.xticks(range(11))
+# plt.yticks(range(11))
+plt.show()
+
+
+
+plt.figure(figsize=(8, 6))
+plt.imshow(dist_max_e, cmap='viridis', interpolation='nearest')
+plt.colorbar(label='Euclidean distance of HKS - Max')
+plt.xlabel('Graph 2')
+plt.ylabel('Graph 1')
+plt.title('Pairwise Distances Heatmap')
+# plt.xticks(range(11))
+# plt.yticks(range(11))
+plt.show()
+
+
+plt.figure(figsize=(8, 6))
+plt.imshow(dist_min_cosine, cmap='viridis', interpolation='nearest')
+plt.colorbar(label='Euclidean distance of HKS - Min')
+plt.xlabel('Graph 2')
+plt.ylabel('Graph 1')
+plt.title('Pairwise Distances Heatmap')
+# plt.xticks(range(11))
+# plt.yticks(range(11))
+plt.show()
+
+
+plt.figure(figsize=(8, 6))
+plt.imshow(dist_saddle_cosine, cmap='viridis', interpolation='nearest')
+plt.colorbar(label='Euclidean distance of HKS - Saddles')
+plt.xlabel('Graph 2')
+plt.ylabel('Graph 1')
+plt.title('Pairwise Distances Heatmap')
+# plt.xticks(range(11))
+# plt.yticks(range(11))
+plt.show()
+
+
+
+plt.figure(figsize=(8, 6))
+plt.imshow(dist_max_cosine, cmap='viridis', interpolation='nearest')
+plt.colorbar(label='Euclidean distance of HKS - Max')
+plt.xlabel('Graph 2')
+plt.ylabel('Graph 1')
+plt.title('Pairwise Distances Heatmap')
+# plt.xticks(range(11))
+# plt.yticks(range(11))
+plt.show()
